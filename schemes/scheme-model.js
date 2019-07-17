8cfa51193f9961db61114861cdeb38818df8be52
Hoxtygen <hoxtygen@live.com>
2019-07-17 13:29:03 +0100

    Get scheme by Id

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -5,8 +5,13 @@ const db = require('../data/dbConfig');
 
 module.exports = {
     find,
+    findById,
 }
 
 function find() {
     return db('schemes')
+}
+
+function findById(id) {
+    return db('schemes').where({ id })
 }
