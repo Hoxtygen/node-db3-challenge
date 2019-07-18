a17fe83cd2380a0645c1d23cbc0072fd8f632267
Hoxtygen <hoxtygen@live.com>
2019-07-18 06:25:57 +0100

    Find steps

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -6,6 +6,7 @@ const db = require('../data/dbConfig');
 module.exports = {
     find,
     findById,
+    findSteps
 }
 
 function find() {
@@ -14,4 +15,11 @@ function find() {
 
 function findById(id) {
     return db('schemes').where({ id })
+}
+
+function findSteps(id) {
+    return db('steps')
+        .join('schemes', 'schemes.id', 'scheme_id')
+        .select('steps.*', 'scheme_name as scheme')
+        where({ 'scheme_id': id})
 }
